af6e8c542680900c9e48cee4551d9bb2f98e7b06
Alexander Wellbrock <a.wellbrock@mailbox.org>
2022-09-17 17:07:30 +0200

    add configuration file and logging config
    We now have a unified way to configure the application. This also brings
    a simple logging configuration of the python native logging module.
    
    To log something simply import the logging module, retrieve a logger and
    start using it:
    
    import logging
    logger = logging.getLogger(__name__)
    
    This will give you a logger prefixed with the module path name such as
    'iou.lib.groups'.

---
+++ iou/config.py
@@ -0,0 +1,67 @@
+"""
+Configuration interface for iou.
+
+Allows configuration from environment variables.
+"""
+
+import logging
+import logging.config
+from enum import Enum
+from importlib.abc import Traversable
+from importlib.resources import files
+from typing import BinaryIO, List, Pattern, Union
+
+import tomli
+from pydantic import AnyHttpUrl, BaseSettings
+
+
+class Environment(Enum):
+    """Runtime environment which configures e.g. certain loggers differently"""
+
+    PROD = "production"
+    DEVELOP = "develop"
+
+
+def load_log_config(config_path: Union[str, Traversable]) -> None:
+    """
+    Loads configuration from a toml file.
+
+    This configuration file contains a standard Python logging configuration
+    dictionary. See [1] for details on the valid settings.
+
+    [1]: https://docs.python.org/3.7/library/logging.config.html#configuration-dictionary-schema
+    """
+    config_file: BinaryIO
+    if isinstance(config_path, Traversable):
+        config_file = config_path.open("rb")
+    else:
+        config_file = open(config_path, "rb")  # pylint: disable=consider-using-with
+    log_config = tomli.load(config_file)
+    if not log_config:
+        logging.warning("Loaded a blank logging config?")
+    config_file.close()
+
+    logging.config.dictConfig(log_config)
+    if settings.IOU_ENVIRONMENT == Environment.DEVELOP:
+        logging.getLogger("root").setLevel(logging.DEBUG)
+
+
+class Settings(BaseSettings):
+    """Primary settings of the app."""
+
+    IOU_SERVER_NAME: str = "iou"
+    IOU_SERVER_HOST: str = "127.0.0.1"
+    IOU_SERVER_PORT: int = 8000
+    IOU_ENVIRONMENT: Environment = Environment.DEVELOP
+
+    IOU_LOG_CONFIG_FILE: Union[str, Traversable] = (
+        files("iou") / "log_config.toml"
+    )
+
+    class Config:
+        # pylint: disable=too-few-public-methods
+        """Static configuration"""
+        case_sensitive = True
+
+
+settings = Settings()
--- iou/main.py
+++ iou/main.py
@@ -1,6 +1,14 @@
+import logging
+
 from fastapi import FastAPI
 
 from iou.api.router import api_router
+from iou.config import load_log_config, settings
+from iou._version import VERSION
+
+load_log_config(settings.IOU_LOG_CONFIG_FILE)
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI(title='IOU')
 
@@ -9,3 +17,11 @@ app.include_router(api_router, prefix='/api')
 @app.get('/')
 async def root() -> dict[str, str]:
     return {'message': 'IOU as in I o(we) (yo)u'}
+
+logger.info(
+    "Started IOU main process in environment '%s' on http://%s:%s",
+    settings.IOU_ENVIRONMENT.value,
+    settings.IOU_SERVER_HOST,
+    settings.IOU_SERVER_PORT,
+)
+logger.info("Version: %s", VERSION)
