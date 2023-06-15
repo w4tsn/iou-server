c1da0ef99d134a1c692a82c91eefbdf1f913f4eb
w4tsn <a.wellbrock@mailbox.org>
2023-06-15 08:27:36 +0200

    add missing alembic migrations

---
+++ alembic/versions/999133aa173f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 999133aa173f
+Revises: 755def1ec563
+Create Date: 2023-06-12 20:38:06.795889
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '999133aa173f'
+down_revision = '755def1ec563'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ alembic/versions/ebc0580f9634_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ebc0580f9634
+Revises: 999133aa173f
+Create Date: 2023-06-12 20:39:50.119983
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ebc0580f9634'
+down_revision = '999133aa173f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
